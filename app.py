22f5e7c737c589be52cefd096418cb0c10e62971
jcunningham3 <jcunningham3@gmail.com>
2021-06-21 14:21:42 -0500

    Continuing to work on this assessment. Working on error handling

--- app.py
+++ app.py
@@ -13,76 +13,115 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 
 
-
 @app.route("/")
 def homepage():
     """Show homepage."""
     return render_template("index.html")
 
-@app.route('/api/lucky')
-# list all todos
-def list_lucky_users():
-    all_users = [user.serialized() for user in Users.query.all()]
-    return jsonify(users=all_users)
 
 @app.route('/api/lucky', methods=['POST'])
-def make_lucky_user():
+def handle_lucky_user():
+
+    # set the incoming header data to data
     data = request.json
 
-    # BACKEND error handling
+    # return name error in JSON
     if data['name'] == '':
-        return jsonify(message='name field must not be blank')
+        return jsonify(
+            {"error":
+                {
+                    "name": ["This field is required."]
+                }
+            })
+    elif isinstance(data['name'], str) == False:
+        return jsonify(
+        {"error":
+            {
+                "name": ["This field is must be a string."]
+            }
+        })
+
+    # return email error in JSON
     elif data['email'] == '':
-        return jsonify(message='name field must not be blank')
-    elif int(data['year']) < 1900:
-        return jsonify(message='year must be an integer bwtween 1900 and 2000')
-    elif int(data['year']) > 2020:
-        return jsonify(message='year must be an integer bwtween 1900 and 2000')
+        return jsonify(
+        {"error":
+            {
+                "email": ["This field is required."]
+            }
+        })
+        if data['email'] == '':
+            return render_template('index.html', email="This field is required.")
+    elif isinstance(data['email'], str) == False:
+        return jsonify(
+        {"error":
+            {
+                "email": ["This field is must be a string."]
+            }
+        })
+
+    # return year error in JSON
+    elif data['year'] == '':
+        return jsonify(
+        {"error":
+            {
+                "year": ["This field is required."]
+            }
+        })
+        if data['year'] == '':
+            return render_template('index.html', year="This field is required.")
+    elif data['year'] < 1900:
+        return jsonify(
+        {"error":
+            {
+                "year": ["This field must be greater than or equal to 1900."]
+            }
+        })
+    elif data['year'] > 2020:
+        return jsonify(
+        {"error":
+            {
+                "year": ["This field must be leass than or equal to 2020."]
+            }
+        })
+
+    # return color error in JSON
     elif data['color'] == '':
-        return jsonify(message='color field must not be blank')
-
-    # OTHERWISE CREATE A USER
+        return jsonify(
+        {"error":
+            {
+                "color": ["This field is required."]
+            }
+        })
+    elif isinstance(data['color'], str) == False:
+        return jsonify(
+        {"error":
+            {
+                "name": ["This field is must be a string."]
+            }
+        })
+    elif data['color'] != 'red':
+        if data['color'] != 'orange':
+            if data['color'] != 'green':
+                if data['color'] != 'blue':
+                    return jsonify(
+                    {"error":
+                        {
+                            "color": ["This field is must red, orange, green, or blue."]
+                        }
+                    })
+    elif data['num_fact'] != '':
+        return jsonify(
+        {"num request":
+            {
+                "num_fact": ["posted successfully"]
+            }
+        })
+    elif data['year_fact'] != '':
+        return jsonify(
+        {"year request":
+            {
+                "year_fact": ["posted successfully"]
+            }
+        })
     else:
-        user = Users(name = data['name'], year = data['year'], email = data['email'], color = data['color'])
-        db.session.add(user)
-        db.session.commit()
-
-        return(jsonify(user=user.serialized()), 201)
-
-@app.route('/purtiy')
-def nothing_func():
-    return
-    """async function processForm(event) {
-      event.preventDefault();
-      //get the name value and check for errors
-      let name = document.querySelector('#name').innerHTML;
-      if(name == ""){
-         document.querySelector('#name-err').innerText = "This field cannot be blank.";
-      }
-      //get the year value and check for errors
-      let year = document.querySelector('#year')..innerHTML;
-      if(year < 1900 || year > 2000){
-         document.querySelector('#year-err').innerText = "Please enter a year between 1900 and 2000.";
-      }
-      //get the email value and check for errors
-      let email = document.querySelector('#email')..innerHTML;
-      if(email == ""){
-         document.querySelector('#email-err').innerText = "This field cannot be blank.";
-      }
-      //get the color value and check for errors
-      let color = document.querySelector('#color')..innerHTML;
-      color = color.toLowerCase()
-      if(color == ""){
-         document.querySelector('#color-err').innerText = "Colors are limited to: red, green, orange, and blue.";
-      }
-      const newLuckyNumResponse = await axios.post('${BASE_URL}/lucky', {
-        name,
-        year,
-        email,
-        color
-      });
-    }
-
-
-    $("#lucky-form").on("submit", processForm);
-    """
+        return data['num_fact']
--- static/lucky.js
+++ static/lucky.js
@@ -1,51 +1,40 @@
 /** processForm: get data from form and make AJAX call to our API. */
-let randnum = Math.floor(Math.random() * 100);
-
 
 $("#lucky-form").on("submit", async function (event) {
   event.preventDefault();
 
+  //collect the form data from index.html with jquery
   let name = $("#name").val();
   let year = $("#year").val();
+  year = parseInt(year)
   let email = $("#email").val();
   let color = $("#color").val();
-  //error handling
-  if(name == "") {
-    document.querySelector("#name-err").innerHTML = "This field cannot be blank.";
-  }
-  else if(year < 1900 || year > 2020) {
-    document.querySelector("#name-err").innerHTML = "";
-    document.querySelector("#year-err").innerHTML = "This field must be an integer between 1900 - 2000.";
-  }
-  else if(email == "") {
-    document.querySelector("#year-err").innerHTML = "";
-    document.querySelector("#email-err").innerHTML = "This field cannot be blank.";
-  }
-  else if(color == "") {
-    document.querySelector("#email-err").innerHTML = "";
-    document.querySelector("#color-err").innerHTML = "This field cannot be blank.";
-  }
-  //if no errors
-  else {
-    let lucky_num_fact = await axios.get(`http://numbersapi.com/${randnum}`);
-    let year_fact = await axios.get(`http://numbersapi.com/${year}`);
-    let data = {
-      name: name,
-      year: year,
-      email: email,
-      color: color,
-      lucky_num_fact: lucky_num_fact,
-      year_fact: year_fact
-    }
-    //request and send data to the api on the BACKEND for processing
-    await axios.post('http://127.0.0.1:5000/api/lucky', data);
 
-    document.querySelector("#lucky-results").innerHTML = "<br>" +
-      "Your Lucky Number is: " + randnum + '<br>' +
-      "Lucky Number Fact: " + data.lucky_num_fact + '<br>' +
-      "Your birth year is " + year + "<br>" +
-      "Birth Year Fact: " + data.year_fact;
+  //make a GET request to the numbers api and set the result to variables
+  let rand_num = Math.floor(Math.random() * 100);
+  let num_fact = await axios.get('http://numbersapi.com/' + rand_num );
+  let year_fact = await axios.get('http://numbersapi.com/' + year);
 
-    $("#lucky-form").trigger("reset");
+  //create a header called data with the form data and the 2 GET requests to be sent as a parameter of the axios post request
+  let data = {
+    name: name,
+    year: year,
+    email: email,
+    color: color,
+    num_fact: num_fact,
+    year_fact: year_fact
   }
+
+  //request and send data to the api on the BACKEND for processing and handle the response and errors
+  let res = await axios.post('http://127.0.0.1:5000/api/lucky', data)
+    .then(function(res){
+      console.log(res)
+      //document.querySelector('#lucky-results').innerHTML = res;
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+
+  //clear the form
+  $("#lucky-form").trigger("reset");
 });
